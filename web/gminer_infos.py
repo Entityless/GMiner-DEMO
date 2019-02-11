bd2d36a6f40153da8635d871cfa0382763c2dfb2
MintYiqingchen <821244608@qq.com>
2019-02-11 16:19:05 +0800

    enable stop button and some trivial fixes

--- web/gminer_infos.py
+++ web/gminer_infos.py
@@ -3,14 +3,14 @@ cd_param = {
         'name':'k-threshold',
         'type':None,
         'help':'',
-        'default': 3,
+        'default': 6,
         }
 
 min_weight = {
         'name': 'min-weight',
         'type': None,
         'help': 'threshold that only add the new candidates into subgraph with weight >= MIN_WEIGHT',
-        'default': 0.92,
+        'default': 0.57,
         }
 
 min_core_size = {
@@ -24,14 +24,14 @@ min_result_size = {
         'name': 'min-result-size',
         'type': None,
         'help':'threshold that only store the result cluster with size >= MIN_RESULT_SIZE',
-        'default': 5
+        'default': 20
         }
 
 diff_ratio = {
         'name': 'diff-ratio',
         'type': None,
         'help': 'threshold for judging two weight is similarity or not',
-        'default': 0.001
+        'default': 0.05
         }
 
 iter_round_max = {
@@ -93,6 +93,19 @@ gminer_persons = [
     {'name': 'Changji Li', 'img': 'lcj.jpg', 'info': ' is currently a Master student in Department of Computer Science and Engineering, Chinese University of Hong Kong. He will pursue MPhil degree in CSE, CUHK as well whose research interests are about distributed computing system and large-scale graph processing.'},
     {'name': 'Jian Zhang', 'img': 'zj.jpg', 'info': ' is a PhD student in the Department of Computer Science and Engineering, The Chinese University of Hong Kong. He is now interested in distributed systems and high-performanced computation.'},
     ]
+
+gminer_compare = [
+    {'slide': 1, 'description': 'We tested only TC and MCF on the four non-attributed graphs, as most of the systems could not handle the other three applications. Table 3 presents the results. Arabesque, Giraph and GraphX either ran out of memory (OOM) or over 24 hours for most of the cases. For the relatively small graphs (i.e., Skitter and Orkut), their performance (even for TC) is significantly worse than G-Miner. The main reasons for their poor performance are because (1) these systems do not have a mechanism to process a large number of subgraphs (e.g., pipelining them to disk) and hence result in OOM, and (2) they all suffer from high synchronization barrier costs. Thus, they cannot scale to handle heavier workloads and larger datasets.'},
+    {'slide': 2, 'description': 'Table 4 reports the elapsed running time, the average CPU utilization, the peak aggregate memory usage, and the aggregate amount of network communication of the cluster. We also list the total number of matched patterns under the dataset names. For this heavier workload, G-Miner is significantly faster than G-thinker for all the graphs. This can be partially explained by GMiner’s much higher CPU utilization. Note that the CPU utilization for Skitter is considerably low because it is the smallest graph and so the workload is not heavy enough to make full use of the resource. G-Miner also reduces the communication load and memory usage by its design of process-level cache shared by all the computing threads.'},
+    {'slide': 3, 'description': 'As shown in Figure 5, although G-thinker also adopts a subgraphcentric computational model, its system design does not allow it to effectively overlap network communication with CPU computation, and CPU cores are waiting for data from network in short intermittent periods throughout the whole process.'},
+    {'slide': 4, 'description': 'In contrast, Figure 6 shows that G-Miner allows all the CPU cores to be highly utilized all the time, as its streamlined task-pipeline continues to feed the computing threads with active tasks to be processed. The design of the LSH-based task priority queue and RCV Cache in G-Miner also effectively reduces the vertex pulling except at the early stage when the cache is not yet filled.'},
+    {'slide': 5, 'description': 'Table 5 reports the elapsed running time and memory usage. Due to the much more complicated candidate filtering and processing of CD and GC, G-Miner used considerably more memory and time to complete the jobs. However, even for these heavy, complicated workloads on graphs with high-dimensional attribute lists, G-Miner still recorded good performance numbers, especially considering that Arabesque, Giraph and GraphX actually obtained worse numbers even for the lightest workload, i.e., TC, as shown in Table 3.'},
+    {'slide': 6, 'description': 'McSherry et al. proposed COST to measure the cost of scalability using a distributed system. COST is defined as the minimum number of cores a parallel/distributed solution used to outperform an optimized single-threaded implementation. Figure 7 reports the result, where the horizontal dotted lines plot the running time of a single-threaded implementation. The COST of G-Miner is 3, 3, 2, 2, respectively, for TC on Skitter, TC on Orkut, GM on Skitter, GM on Orkut. These numbers are much lower than the systems measured in Frank McSherry\'s article: Scalability! But at what COST?'},
+    {'slide': 7, 'description': 'Figure 8 and Figure 9 show that G-Miner achieves good speedups in most of the cases, especially for the heavier workload GM on Friendster. G-Miner’s good scalability is due to two reasons: (1) our task-pipeline design enables the computing threads to totally focus on the local task processing without being interrupted by the network communication requests, which matches the computationintensive nature of graph mining workloads; (2) graph partitioning and task stealing enable good load balancing among the nodes.'},
+    {'slide': 8, 'description': 'Figure 8 and Figure 9 show that G-Miner achieves good speedups in most of the cases, especially for the heavier workload GM on Friendster. G-Miner’s good scalability is due to two reasons: (1) our task-pipeline design enables the computing threads to totally focus on the local task processing without being interrupted by the network communication requests, which matches the computationintensive nature of graph mining workloads; (2) graph partitioning and task stealing enable good load balancing among the nodes.'},
+    {'slide': 9, 'description': 'We also show the scalability of the four systems we compared with in Figure 10 as a reference. The figure shows that without a good system design and load balancing mechanism, there is no guarantee on the system scalability. Note that we were only able to measure the scalability for these systems on the smaller datasets using TC for reasons such as OOM and time limit.'},
+    ]
+
 gminer_sysconfig = [
     cache_size, num_comp_th, pipe_pop_num, pop_num, subg_size_t 
 ]
--- web/utils/ini_generator.py
+++ web/utils/ini_generator.py
@@ -7,27 +7,6 @@ import subprocess
 import io
 import time
 
-space_strs = []
-for i in range(1000):
-    space_strs.append("")
-    for j in range(i):
-        space_strs[i] = space_strs[i] + " "
-
-#run and confirm?
-
-def get_frontend_dic():
-
-    param_dic = {}
-    param_dic['dataset'] = 'skitter'
-    param_dic['apps'] = 'tc'
-    param_dic['cache-size'] = '2333'
-    param_dic['num-comp-thread'] = '23333'
-    param_dic['pipe-pop-num'] = '233333'
-    param_dic['pop-num'] = '2333333'
-    param_dic['subg-size-t'] = '23333333'
-
-    return param_dic
-
 input_path = {}
 input_path['skitter'] = {}
 input_path['youtube'] = {}
@@ -56,17 +35,15 @@ input_path['youtube']['cd'] = '/chhuang/gm_input/youtube_attr_10/'
 input_path['orkut']['cd'] = '/chhuang/gm_input/orkut_attr_10/'
 input_path['tencent']['cd'] = '/chhuang/gm_input/tencent_attr_10/'
 
-input_path['skitter']['fco'] = '/chhuang/gm_input/skitter_fco_10/'
-input_path['youtube']['fco'] = '/chhuang/gm_input/youtube_fco_10/'
-input_path['orkut']['fco'] = '/chhuang/gm_input/orkut_fco_10/'
-input_path['dblp']['fco'] = '/chhuang/gm_input/dblp_fco_10/'
+input_path['skitter']['fco'] = '/chhuang/gm_input/skitter_focus_10/'
+input_path['youtube']['fco'] = '/chhuang/gm_input/youtube_focus_10/'
+input_path['orkut']['fco'] = '/chhuang/gm_input/orkut_focus_10/'
+input_path['dblp']['fco'] = '/chhuang/gm_input/dblp_focus_10/'
 
+machine_file = {False : 'machines.cfg', 'ib' : 'ib_machines.cfg'}
 
 def gminer_ini_gen(param_dic):
-    cmd = "mpiexec -n 11 -f machines.cfg $GMINER_HOME/release/"
-    ini_str = ""
 
-    #### gen ini str
     ini_str = """#Copyright 2018 Husky Data Lab, CUHK
 #Authors: Hongzhi Chen, Miao Liu
 
@@ -78,76 +55,55 @@ def gminer_ini_gen(param_dic):
 ;for application I/O and local temporary storage
 HDFS_HOST_ADDRESS = master
 HDFS_PORT = 9000
-"""
-    ini_str += "HDFS_INPUT_PATH = " + input_path[param_dic['dataset']][param_dic['apps']] + '\n'
-    ini_str += """HDFS_OUTPUT_PATH = /chhuang/gm_output/gminer_default_output/
+HDFS_INPUT_PATH = {}
+HDFS_OUTPUT_PATH = /chhuang/gm_output/gminer_default_output/
 LOCAL_TEMP_PATH = /tmp
 FORCE_WRITE = TRUE    ;force to write HDFS_OUTPUT_PATH
-DEMO_LOG_PATH = /home/cghuan/gminer_demo_log/
+DEMO_LOG_PATH = /home/cghuan/gminer_demo_log/ ;change this if needed
 
 [COMPUTING]
 ;for task computing configurations
-"""
-    ini_str += "CACHE_SIZE = " + str(param_dic['cache-size']) + "  ;the size of cachetable in each worker\n"
-    ini_str += "NUM_COMP_THREAD = " + str(param_dic['num-comp-thread']) + "   ;number of threads in threadpool for task computation\n"
-    ini_str += "PIPE_POP_NUM = " + str(param_dic['pipe-pop-num']) + "    ;number of tasks popped out each batch in the pipeline\n"
-    ini_str += """
+CACHE_SIZE = {}  ;the size of cachetable in each worker
+NUM_COMP_THREAD = {}  ;number of threads in threadpool for task computation
+PIPE_POP_NUM = {}  ;number of tasks popped out each batch in the pipeline
+
 [STEALING]
 ;for task stealing configurations
-"""
-    ini_str += "POP_NUM = " + str(param_dic['pop-num']) + "         ;number of tasks for pque pops tasks to remote worker during one stealing procedure\n"
-    ini_str += "SUBG_SIZE_T = " + str(param_dic['subg-size-t']) + "      ;threshold that task can be moved to other workers only if its current subgraph size <= SUBG_SIZE_T\n"
-    ini_str += """LOCAL_RATE = 0.5      ;threshold that task can be moved to other workers only if its current local rate <= LOCAL_RATE
+POP_NUM = {}  ;number of tasks for pque pops tasks to remote worker during one stealing procedure
+SUBG_SIZE_T = {}  ;threshold that task can be moved to other workers only if its current subgraph size <= SUBG_SIZE_T
+LOCAL_RATE = 0.5  ;threshold that task can be moved to other workers only if its current local rate <= LOCAL_RATE
 
 [SYNC]
 ;for context and aggregator sync
 AGG_SLEEP_TIME = 0        ;unit:second; do context and aggregator sync periodically during computation; if AGG_SLEEP_TIME == 0, then no sync happens during computation
 SYS_SLEEP_TIME = 1        ;unit:second; ; do system sync
 
-"""
+""".format(input_path[param_dic['dataset']][param_dic['apps']], str(param_dic['cache-size']), 
+           str(param_dic['num-comp-thread']), str(param_dic['pipe-pop-num']), 
+           str(param_dic['pop-num']), str(param_dic['subg-size-t']))
+
     if(param_dic['apps'] == 'tc'):
-        #
-        cmd += 'tc '
+        app_cmd = 'tc'
 
     if(param_dic['apps'] == 'mc'):
-        #
-        cmd += 'mc '
+        app_cmd = 'mc '
 
     if(param_dic['apps'] == 'gm'):
-        #
-        cmd += 'gm '
+        app_cmd = 'gm '
 
     if(param_dic['apps'] == 'cd'):
-        #
-        cmd += 'cd '
-        cmd += str(param_dic['k-threshold'])
+        app_cmd = 'cd {}'.format(str(param_dic['k-threshold']))
 
     if(param_dic['apps'] == 'fco'):
-        #
-        cmd += 'fco '
-        cmd += str(param_dic['min-weight']) + ' '
-        cmd += str(param_dic['min-core-size']) + ' '
-        cmd += str(param_dic['min-result-size']) + ' '
-        cmd += str(param_dic['diff-ratio']) + ' '
-        cmd += str(param_dic['iter-round-max']) + ' '
-        cmd += str(param_dic['cand-max-time']) + ' '
-
+        app_cmd = 'fco {} {} {} {} {} {} '.format(str(param_dic['min-weight']), str(param_dic['min-core-size']), str(param_dic['min-result-size']),
+                                                 str(param_dic['diff-ratio']), str(param_dic['iter-round-max']), str(param_dic['cand-max-time']))
 
     #### gen cmd
 
-    cmd += ' | tee /dev/shm/chhuang/merge-gminer/{}.log'
+    # cmd = "mpiexec -n 11 -f {} $GMINER_HOME/release/{} {}".format(machine_file[param_dic['ib']], app_cmd,
+    #                                                               ' | tee /dev/shm/chhuang/merge-gminer/{}.log')
+    cmd = "mpiexec -n 11 -f {} $GMINER_HOME/release/{}".format(machine_file[param_dic['ib']], app_cmd)
+    # cmd = "mpiexec -n 11 -f {} $GMINER_HOME/release/{} {}".format(machine_file[param_dic['ib']], app_cmd,
+    #                                                               ' > /dev/shm/chhuang/merge-gminer/{}.log')
 
     return [cmd, ini_str]
-
-if __name__ == "__main__":
-
-    param_dic = get_frontend_dic()
-    #rsync
-    #cp master
-    [run_cmd, ini_str] = gminer_ini_gen(param_dic)
-
-    print(run_cmd)
-
-    with open('faker.ini', 'w') as f:
-        f.write(ini_str)
-        f.close()
